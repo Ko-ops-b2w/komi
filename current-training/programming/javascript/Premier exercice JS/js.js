e4c7d456d5fb765e5b0a8fa590c0edfd2ff4a150
Komi alias Born2win <senyokomlavi78@gmail.com>
2025-05-27 15:55:53 +0200

    js

---
+++ current-training/programming/javascript/Premier exercice JS/js.js
@@ -0,0 +1,37 @@
+let age = 30;
+const majeur = 18;
+
+console.log(age > majeur);
+
+let nmbrea = 5;
+let nmbreb = 3;
+console.log(nmbrea + nmbreb);
+let nmbrec = 10;
+let nmbred = 7;
+console.log(nmbrec - nmbred);
+let nmbree = 4;
+let nmbref = 6; 
+console.log(nmbree * nmbref);
+
+let prenom = " John";
+let nom = "Doe"
+nomComplet = nom + prenom;
+console.log(nomComplet);
+
+let sujet = "Le chat";
+let verbe = " mange";
+let objet = " la souris";
+phrase = sujet + verbe + objet;
+console.log(phrase);
+
+function saluer(Alice){
+    console.log(Alice)
+}
+saluer("Bonjour,Alice!")
+
+function multiplication(a, b){
+    return a * b;
+}
+
+console.log(multiplication(7,8));
+console.log(multiplication(9,5));
